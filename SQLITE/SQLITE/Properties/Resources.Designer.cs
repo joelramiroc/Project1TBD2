e37f04385f2e14d258e71d55ae6ccf250782ff14
joelramiroc <www.joelramiro@unitec.edu>
2018-08-03 04:53:50 -0600

    creacion de DataBaseManager.cs, abrir databases de sqlite, listar tablas y vistas. cerrar conexion

--- SQLITE/SQLITE/Properties/Resources.Designer.cs
+++ SQLITE/SQLITE/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace SQLITE.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap play {
             get {
                 object obj = ResourceManager.GetObject("play", resourceCulture);
